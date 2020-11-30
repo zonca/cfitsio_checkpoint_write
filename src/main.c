448bbbb8788c9324e2e9e4cbb2574deeeabf0a6c
Andrea Zonca <code@andreazonca.com>
2020-11-30 12:39:38 -0800

    write image HDU instead of table

--- src/main.c
+++ src/main.c
@@ -36,56 +36,63 @@ int main()
 	printf("Hello World!\n");
 
     fitsfile *fptr;       /* pointer to the FITS file, defined in fitsio.h */
-    int status, hdutype;
-    long firstrow, firstelem;
-
-    int tfields   = 3;       /* table will have 3 columns */
-    long nrows    = 6;       /* table will have 6 rows    */
-
     char filename[] = "test_cube.fits";           /* name for new FITS file */
-    char extname[] = "PLANETS_Binary";           /* extension name */
+    int status, ii, jj, hdutype;
+    long  fpixel, nelements;
+    unsigned short *array[200];
 
-    /* define the name, datatype, and physical units for the 3 columns */
-    char *ttype[] = { "Planet", "Diameter", "Density" };
-    char *tform[] = { "8a",     "1J",       "1E"    };
-    char *tunit[] = { "\0",      "km",       "g/cm"    };
+    /* initialize FITS image parameters */
+    int bitpix   =  USHORT_IMG; /* 16-bit unsigned short pixel values       */
+    long naxis    =   2;  /* 2-dimensional image                            */    
+    long naxes[2] = { 300, 200 };   /* image is 300 pixels wide by 200 rows */
 
-    /* define the name diameter, and density of each planet */
-    char *planet[] = {"Mercury", "Venus", "Earth", "Mars","Jupiter","Saturn"};
-    long  diameter[] = {4880,     12112,   12742,   6800,  143000,   121000};
-    float density[]  = { 5.1f,      5.3f,     5.52f,   3.94f,   1.33f,     0.69f};
+    /* allocate memory for the whole image */ 
+    array[0] = (unsigned short *)malloc( naxes[0] * naxes[1]
+                                        * sizeof( unsigned short ) );
 
-    status=0;
-	
+    /* initialize pointers to the start of each row of the image */
+    for( ii=1; ii<naxes[1]; ii++ )
+      array[ii] = array[ii-1] + naxes[0];
+
     remove(filename);               /* Delete old file if it already exists */
 
-    /* open the FITS file containing a primary array and an ASCII table */
-    if ( fits_create_file(&fptr, filename, &status) ) 
-         printerror( status );
-	
+    status = 0;         /* initialize status before calling fitsio routines */
+
+    if (fits_create_file(&fptr, filename, &status)) /* create new FITS file */
+         printerror( status );           /* call printerror if error occurs */
+
 	create_empty_hdu(fptr);
-
-    if ( fits_movabs_hdu(fptr, 1, &hdutype, &status) ) /* move to 1st HDU */
+ 
+    if ( fits_movabs_hdu(fptr, 1, &hdutype, &status) ) /* move to 2nd HDU */
          printerror( status );
+    
+	/* write the required keywords for the primary array image.     */
+    /* Since bitpix = USHORT_IMG, this will cause cfitsio to create */
+    /* a FITS image with BITPIX = 16 (signed short integers) with   */
+    /* BSCALE = 1.0 and BZERO = 32768.  This is the convention that */
+    /* FITS uses to store unsigned integers.  Note that the BSCALE  */
+    /* and BZERO keywords will be automatically written by cfitsio  */
+    /* in this case.                                                */
 
-    /* append a new empty binary table onto the FITS file */
-    if ( fits_create_tbl( fptr, BINARY_TBL, nrows, tfields, ttype, tform,
-                tunit, extname, &status) )
-         printerror( status );
+    if ( fits_create_img(fptr,  bitpix, naxis, naxes, &status) )
+         printerror( status );          
 
-    firstrow  = 1;  /* first row in table to write   */
-    firstelem = 1;  /* first element in row  (ignored in ASCII tables) */
+    /* initialize the values in the image with a linear ramp function */
+    for (jj = 0; jj < naxes[1]; jj++)
+    {   for (ii = 0; ii < naxes[0]; ii++)
+        {
+            array[jj][ii] = ii + jj;
+        }
+    }
 
-    /* write names to the first column (character strings) */
-    /* write diameters to the second column (longs) */
-    /* write density to the third column (floats) */
+    fpixel = 1;                               /* first pixel to write      */
+    nelements = naxes[0] * naxes[1];          /* number of pixels to write */
 
-    fits_write_col(fptr, TSTRING, 1, firstrow, firstelem, nrows, planet,
-                   &status);
-    fits_write_col(fptr, TLONG, 2, firstrow, firstelem, nrows, diameter,
-                   &status);
-    fits_write_col(fptr, TFLOAT, 3, firstrow, firstelem, nrows, density,
-                   &status);
+    /* write the array of unsigned integers to the FITS file */
+    if ( fits_write_img(fptr, TUSHORT, fpixel, nelements, array[0], &status) )
+        printerror( status );
+      
+    free( array[0] );  /* free previously allocated memory */
 
     if ( fits_close_file(fptr, &status) )       /* close the FITS file */
          printerror( status );
