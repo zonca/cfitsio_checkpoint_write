6b37612cfddbc1af1b58de7bfa5fd7a414c1942b
Andrea Zonca <code@andreazonca.com>
2020-11-30 14:57:30 -0800

    write multiple readouts with different offset

--- read_fits.py
+++ read_fits.py
@@ -1,8 +1,15 @@
 from astropy.io import fits
+import numpy as np
 
 filename = "test_cube.fits"
 
+readout_shape = (30,20)
+num_readouts = 500
 with fits.open(filename) as f:
     print("shape", f[1].data.shape)
-    assert f[1].data.shape == (20,30,500)
-    print("data", f[1].data.flat[:5], f[1].data.flat[-5:])
+    assert f[1].data.shape == (num_readouts,) + readout_shape
+    for i_readout in range(10):
+        np.testing.assert_array_equal(
+            f[1].data[i_readout, :, :],
+            np.arange(i_readout*10000, i_readout*10000 + readout_shape[0]*readout_shape[1]).reshape(readout_shape)
+        )
--- src/main.c
+++ src/main.c
@@ -33,20 +33,24 @@ static int create_empty_hdu(fitsfile * fptr)
 
 int main()
 {
-	printf("Hello World!\n");
+    printf("Hello World!\n");
 
     fitsfile *fptr;       /* pointer to the FITS file, defined in fitsio.h */
     char filename[] = "test_cube.fits";           /* name for new FITS file */
-    int status, ii, jj, hdutype;
-    long  fpixel, nelements;
-    unsigned int *readout[600];
-
+    int status, ii, jj, kk, hdutype;
+    long  nelements;
+    long fpixel[3];
     /* initialize FITS image parameters */
     int bitpix   =  ULONG_IMG; /* 32-bit unsigned short pixel values       */
     long naxis    =   3;  /* 3-dimensional image                            */
-	long naxes[3] = { 500, 30, 20 };   /* image is 300 pixels wide by 200 rows */
+    static long num_readouts = 500;
+    static long xsize = 20;
+    static long ysize = 30;
+    long naxes[3] = { xsize, ysize, num_readouts };
+    unsigned int *readout[xsize*ysize];
 
-    /* allocate memory for the whole image */ 
+    long num_readouts_written = 10;
+    /* allocate memory for the whole image */
     readout[0] = (unsigned int *)malloc( naxes[0] * naxes[1]
                                         * sizeof( unsigned int ) );
 
@@ -61,12 +65,12 @@ int main()
     if (fits_create_file(&fptr, filename, &status)) /* create new FITS file */
          printerror( status );           /* call printerror if error occurs */
 
-	create_empty_hdu(fptr);
- 
+    create_empty_hdu(fptr);
+
     if ( fits_movabs_hdu(fptr, 1, &hdutype, &status) ) /* move to 2nd HDU */
          printerror( status );
-    
-	/* write the required keywords for the primary array image.     */
+
+    /* write the required keywords for the primary array image.     */
     /* Since bitpix = USHORT_IMG, this will cause cfitsio to create */
     /* a FITS image with BITPIX = 16 (signed short integers) with   */
     /* BSCALE = 1.0 and BZERO = 32768.  This is the convention that */
@@ -75,28 +79,42 @@ int main()
     /* in this case.                                                */
 
     if ( fits_create_img(fptr,  bitpix, naxis, naxes, &status) )
-         printerror( status );          
+         printerror( status );
 
     /* initialize the values in the image with a linear ramp function */
+    kk = 0;
     for (jj = 0; jj < naxes[1]; jj++)
     {   for (ii = 0; ii < naxes[0]; ii++)
         {
-            readout[jj][ii] = ii + jj;
+            readout[jj][ii] = kk;
+            kk++;
         }
     }
 
-    fpixel = 1;                               /* first pixel to write      */
-    nelements = naxes[0] * naxes[1];          /* number of pixels to write */
+    nelements = xsize * ysize;          /* number of pixels to write */
 
     /* write the array of unsigned integers to the FITS file */
-    if ( fits_write_img(fptr, TUINT, fpixel, nelements, readout[0], &status) )
-        printerror( status );
-      
+    for (kk = 0; kk < 10; kk++) {
+        fpixel[0] = 1;    /* first pixel to write      */
+        fpixel[1] = 1;
+        fpixel[2] = kk + 1;
+
+        if ( fits_write_pix(fptr, TUINT, fpixel, nelements, readout[0], &status) )
+            printerror( status );
+
+          for (jj = 0; jj < naxes[1]; jj++)
+        {   for (ii = 0; ii < naxes[0]; ii++)
+            {
+                readout[jj][ii] += 10000;
+            }
+        }
+    }
     free( readout[0] );  /* free previously allocated memory */
+
 
     if ( fits_close_file(fptr, &status) )       /* close the FITS file */
          printerror( status );
     return;
 
-	return (0);
+    return (0);
 }
